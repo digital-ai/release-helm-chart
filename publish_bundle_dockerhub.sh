f70db9b79a0474c96279f99b02da3c1ec0938eb0
Vedran Pugar <vpugar@digital.ai>
2024-08-13 09:53:05 +0200

    D-35529 improve build env checks to avoid common problems

--- publish_bundle_dockerhub.sh
+++ publish_bundle_dockerhub.sh
@@ -1,1 +1,15 @@
+#!/usr/bin/env bash
+
+# Check if the value ends with 'redhat-slim'
+if [[ "$RELEASE_APP_EXPLICIT" != *"-redhat-slim" ]]; then
+  echo "The '\$RELEASE_APP_EXPLICIT=$RELEASE_APP_EXPLICIT' environment variable does not end with '-redhat-slim'."
+fi
+if [ -z "$RELEASE_REGISTRY" ]; then
+  echo "The '\$RELEASE_REGISTRY environment variable is not set."
+  exit 1
+fi
+if [ -z "$RELEASE_EXPLICIT" ]; then
+  echo "The '\$RELEASE_EXPLICIT environment variable is not set."
+  exit 1
+fi
 ./gradlew clean publishBundleToDockerHub -x updateDocs -x buildReadme -x test --stacktrace --info
