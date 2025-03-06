c0ea469cc33513e88b7ecbc78c1a14563b359108
Vedran Pugar <vedran.pugar@digital.ai>
2025-03-06 10:57:51 +0100

    S-118874 Backport Postgresql and RabbitMQ helm chart versions to 24.3.x

--- scripts/upgrade-helm-charts.sh
+++ scripts/upgrade-helm-charts.sh
@@ -2,5 +2,5 @@ SCRIPT_DIR=$( dirname -- "$0"; )
 HELM_DIR="${SCRIPT_DIR}/.."
 DEFAULT_VALUES="$1"
 shift 1
-helm upgrade --install ${DAI_RELEASE} "$HELM_DIR" -n $DAI_NAMESPACE --values "$DEFAULT_VALUES" "$@"
+helm upgrade --install ${DAI_RELEASE} "$HELM_DIR" --create-namespace -n $DAI_NAMESPACE --values "$DEFAULT_VALUES" "$@"
 
