9cd06ff4e7b84e1a74416fb788a9ff72217236e5
Vedran Pugar <72854396+vpugar-digital@users.noreply.github.com>
2025-03-18 10:36:43 +0100

    D-39229 add support to display all released versions (#72)

---
+++ operator/scripts/generate_skip_versions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Input version
+input_version=$1
+csv_file=$2
+
+# Extract major, minor, and patch versions
+IFS='.' read -r major minor patch <<< "$input_version"
+
+# Generate versions from 0 to patch-1
+versions=""
+for ((i=0; i<patch; i++)); do
+  versions+="digitalai-release-operator.v$major.$minor.$i"$'\n'
+done
+
+# Convert versions to a YAML list format
+versions_yaml=$(echo "$versions" | grep -v '^$' | jq -R -s -c 'split("\n")[:-1]')
+
+# Add the versions list under spec.skips in the CSV file using yq
+yq -i ".spec.skips = $versions_yaml" "$csv_file"
