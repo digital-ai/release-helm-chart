9f1dd8a76e663924891ef3a18359304e300c7890
d-venkat <85864739+d-venkat@users.noreply.github.com>
2025-06-17 01:13:44 +0530

    [24.1] S-119674 Removed postgresql and rabbitmq hotfix for seLinuxOptions volumePermissions (#92)
    * [24.1] S-119674 Removed postgresql and rabbitmq hotfix for seLinuxOptions volumePermissions
    
    * [24.1] S-119674 Set in postgresql and rabbitmq security context constraint, seLinuxContext.type: RunAsAny to allow in the respective initContainers securityContext.seLinuxOptions: {}

--- build.gradle.kts
+++ build.gradle.kts
@@ -278,7 +278,7 @@ tasks {
 
     register<Exec>("runHelmLint") {
         group = "helm-test"
-        dependsOn("prepareHelmDeps", "prepareHelmDepsHotfix")
+        dependsOn("prepareHelmDeps")
 
         workingDir(buildXlrOperatorDir)
         commandLine(helmCli, "lint", "-f", "../../../tests/values/basic.yaml")
@@ -290,7 +290,7 @@ tasks {
 
     register<Exec>("installHelmUnitTestPlugin") {
         group = "helm-test"
-        dependsOn("prepareHelmDepsHotfix")
+        dependsOn("prepareHelmDeps")
 
         workingDir(buildXlrOperatorDir)
         commandLine(helmCli, "plugin", "list")
@@ -332,7 +332,7 @@ tasks {
 
     register<Exec>("buildHelmPackage") {
         group = "helm"
-        dependsOn("prepareHelmDepsHotfix")
+        dependsOn("prepareHelmDeps")
         workingDir(buildXlrDir)
         commandLine(helmCli, "package", "--app-version=$releasedAppVersion", project.name)
 
@@ -402,7 +402,7 @@ tasks {
 
     register<Exec>("buildOperatorImage") {
         group = "operator"
-        dependsOn("installKustomize", "buildOperatorApiHotfix")
+        dependsOn("installKustomize", "buildOperatorApi")
         workingDir(buildXlrDir)
         commandLine("make", "docker-build",
             "IMG=$operatorImageUrl", operatorSdkCliVar, kustomizeCliVar)
@@ -472,7 +472,7 @@ tasks {
 
     register<Exec>("buildOperatorBundle") {
         group = "operator-bundle"
-        dependsOn("installKustomize", "buildOperatorApiHotfix")
+        dependsOn("installKustomize", "buildOperatorApi")
         workingDir(buildXlrDir)
         commandLine("make", "bundle",
             "IMG=$operatorImageUrl", "BUNDLE_GEN_FLAGS=--overwrite --version=$releasedVersion --channels=$operatorBundleChannels --package=digitalai-release-operator --use-image-digests",
@@ -690,61 +690,6 @@ tasks {
     register<GenerateDocumentation>("updateDocs") {
         group = "docusaurus"
         dependsOn(named("docBuild"))
-    }
-
-    val postgresqlSubchart = "postgresql-15.4.0.tgz"
-
-    register("prepareHelmDepsHotfix") {
-        group = "helm-hotfix"
-        dependsOn(
-            named("prepareHelmDeps")
-        )
-    }
-
-    // hotfix operator
-    val operatorChartDir = layout.buildDirectory.dir("xlr/helm-charts/digitalai-release/charts")
-
-    // postgresql
-    val postgresqlOperatorChart = operatorChartDir.get().file(postgresqlSubchart)
-
-    register<Exec>("hotfixPostgresqlOperatorChart") {
-        group = "operator-hotfix"
-        dependsOn(named("buildOperatorApi"))
-        doFirst {
-            copy {
-                from(tarTree(postgresqlOperatorChart))
-                into(operatorChartDir.get())
-            }
-            delete(postgresqlOperatorChart)
-        }
-        workingDir(operatorChartDir.get())
-        commandLine("yq", "-i",
-            ".volumePermissions.containerSecurityContext.seLinuxOptions=null", "postgresql/values.yaml")
-
-        doLast {
-            logger.lifecycle("Hotfix Postgresql operator helm chart")
-        }
-    }
-
-    register<Tar>("hotfixPostgresqlOperatorChartPackage") {
-        group = "operator-hotfix"
-        dependsOn(named("hotfixPostgresqlOperatorChart"))
-        from(operatorChartDir)
-        include("postgresql/**")
-        archiveFileName.set(postgresqlSubchart)
-        destinationDirectory.set(file(operatorChartDir))
-        compression = Compression.GZIP
-    }
-
-    register("buildOperatorApiHotfix") {
-        group = "operator-hotfix"
-        dependsOn(
-            named("hotfixPostgresqlOperatorChartPackage"),
-            named("buildOperatorApi")
-        )
-        doLast {
-            delete(operatorChartDir.get().dir("postgresql"))
-        }
     }
 
     register<Exec>("preflightCheckOperator") {
